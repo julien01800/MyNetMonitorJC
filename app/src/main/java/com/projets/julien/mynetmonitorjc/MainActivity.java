4046c6c4777662ed6e1d7d298df8fdbdb929aa2d
julien_Couzin <julien.couzin@gmail.com>
2016-07-20 22:10:49 +0200

    Add base projet

---
+++ app/src/main/java/com/projets/julien/mynetmonitorjc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.projets.julien.mynetmonitorjc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
